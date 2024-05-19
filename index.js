de76bf1cc3a271a82b002d0d810df57c383e7e93
eversardoth <68908705+eversardoth@users.noreply.github.com>
2024-05-19 17:08:31 -0500

    add request and response logs

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ app.use(cors());
 app.get('/temperature-conversion', (req, res) => {
   const amount = req.query.amount;
 
+  console.log(`Received request with temperature: ${amount}°C`);
+
   if (!amount || isNaN(amount)) {
     return res.status(400).json({ error: 'Invalid temperature. Make sure you are sending a number.' });
   }
 
   const celsius = parseFloat(amount);
   const fahrenheit = (celsius * 9/5) + 32;
+
+  console.log(`Sending response with temperature: ${fahrenheit}°F`);
 
   res.json({ temperature: fahrenheit });
 });
